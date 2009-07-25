ef8d0cb71784d60b349b85ce9b55f193667b4e72
unknown <main@.(none)>
2009-07-25 13:53:27 -0400

    fixed unreachable code warning, added ascentstats class (incomplete, will not compile

---
+++ IRC bot/Functions/AscentStats.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Xml;
+
+namespace IRC_Bot
+{
+    class AscentStats
+    {
+
+        public AscentStats(string location)
+        {
+
+        }
+
+        #region factions
+        public int GetHorde()
+        {
+
+        }
+
+        public int GetAlliance()
+        {
+
+        }
+        #endregion
+        public int GetTotalPlayers()
+        {
+
+        }
+
+        #region server
+        public string GetMemoryUsage()
+        {
+
+        }
+
+        public string GetCPU()
+        {
+
+        }
+
+
+        public string GetLatency()
+        {
+
+        }
+
+
+        #endregion
+
+        #region races
+        public int GetHuman()
+        {
+
+        }
+
+        public int GetDwarf()
+        {
+
+        }
+
+        public int GetNightElf()
+        {
+
+        }
+
+        public int GetGnome()
+        {
+
+        }
+
+        public int GetOrc()
+        {
+
+        }
+
+        public int GetUndead()
+        {
+
+        }
+
+        public int GetTauren()
+        {
+
+        }
+
+        public int GetTroll()
+        {
+
+        }
+
+        public int GetBloodElf()
+        {
+
+        }
+
+        public int GetDraenei()
+        {
+
+        }
+
+        #endregion
+
+        #region classes
+        public int GetWarrior()
+        {
+
+        }
+
+        public int GetHunter()
+        {
+
+        }
+
+        public int GetPriest()
+        {
+
+        }
+
+        public int GetMage()
+        {
+
+        }
+
+        public int GetWarlock()
+        {
+
+        }
+
+        public int GetPaladin()
+        {
+
+        }
+
+        public int GetRogue()
+        {
+
+        }
+
+        public int GetShaman()
+        {
+
+        }
+
+        public int GetDruid()
+        {
+
+        }
+
+        public int GetDeathKnight()
+        {
+
+        }
+        #endregion
+        public int GetGMCount()
+        {
+
+        }
+
+
+    }
+}
